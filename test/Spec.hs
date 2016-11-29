e425d89dfe933cfa143176ba86d3775f0fbcdff2
Pavel Yakovlev <zhmactep@gmail.com>
2016-11-30 00:02:38 +0300

    First tests added

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,2 +1,16 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+import Data.Hex
+import Test.Hspec
+import Test.QuickCheck
+import Database.Bolt.PackStream
+
 main :: IO ()
-main = putStrLn "Test suite not yet implemented"
+main = hspec $
+         describe "PackStream" $ do
+           it "packs integers correct" $ do
+             hex (pack (1::Int)) `shouldBe` "01"
+             hex (pack (1234::Int)) `shouldBe` "C904D2"
+           it "packs doubles correct" $ do
+             hex (pack (6.283185307179586::Double)) `shouldBe` "C1401921FB54442D18"
+             return ()
