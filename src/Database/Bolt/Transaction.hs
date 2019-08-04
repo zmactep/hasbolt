2edb2f27da9bf71f53b016ff6d027b7f6377e92e
Gabriel Volpe <volpegabriel@gmail.com>
2019-08-03 23:50:30 -0500

    Making transact_ more efficient

--- src/Database/Bolt/Transaction.hs
+++ src/Database/Bolt/Transaction.hs
@@ -10,10 +10,12 @@ where
 import           Database.Bolt.Connection.Pipe
 import           Database.Bolt.Connection.Type
 import           Database.Bolt.Connection       ( BoltActionT
+                                                , queryP_
                                                 , queryP'
                                                 , run
                                                 )
 import           Database.Bolt.Record
+import           Database.Bolt.Value.Type
 
 import           Control.Exception              ( IOException )
 import           Control.Monad                  ( join )
@@ -26,6 +28,7 @@ import           Control.Monad.IO.Class         ( MonadIO(..)
                                                 )
 import           Control.Monad.Trans.Reader     ( ask )
 import           Data.Functor                   ( void )
+import           Data.Map                       ( Map )
 import           Data.Map.Strict                ( empty )
 import           Data.Text                      ( Text )
 
@@ -34,22 +37,28 @@ instance Exception TxError
 
 -- |Runs a list of cypher queries with parameters as an atomic operation and returns all the records
 transact :: MonadIO m => [Cypher] -> BoltActionT m [Record]
-transact cyphers = do
-  pipe <- ask
-  liftIO $ txBegin pipe
-  liftIO $ catch (join <$> traverse (sendCypher pipe) cyphers <* void (txCommit pipe))
-                 (\TxError -> txRollback pipe >> throwM TxError)
- where
-  handler :: IOException -> IO [Record]
-  handler e = print e >> throwM TxError
-  sendCypher :: Pipe -> Cypher -> IO [Record]
-  sendCypher pipe c = do
-    putStrLn $ "Sending Cypher " ++ show (cypherQuery c)
-    catch (run pipe $ queryP' (cypherQuery c) (cypherParams c)) handler
+transact = transaction False
 
 -- |Runs a list of cypher queries with parameters as an atomic operation and discards all outputs
 transact_ :: MonadIO m => [Cypher] -> BoltActionT m ()
-transact_ = void . transact
+transact_ = void . transaction True
+
+transaction :: MonadIO m => Bool -> [Cypher] -> BoltActionT m [Record]
+transaction discard cyphers = do
+  pipe <- ask
+  liftIO $ txBegin pipe
+  liftIO $ catch
+    (join <$> traverse (sendCypher pipe) cyphers <* void (txCommit pipe))
+    (\TxError -> txRollback pipe >> throwM TxError)
+ where
+  handler :: IOException -> IO [Record]
+  handler e = print e >> throwM TxError
+  txQuery :: Text -> Map Text Value -> BoltActionT IO [Record]
+  txQuery q p = if discard then [] <$ queryP_ q p else queryP' q p
+  sendCypher :: Pipe -> Cypher -> IO [Record]
+  sendCypher pipe c = do
+    putStrLn $ "Sending Cypher " ++ show (cypherQuery c)
+    catch (run pipe $ txQuery (cypherQuery c) (cypherParams c)) handler
 
 txCommand :: Text -> Pipe -> IO Response
 txCommand cmd pipe = do
